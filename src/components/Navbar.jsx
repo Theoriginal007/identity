80a6e27ca715721a79fd451d79da970624aae7fc
Theoriginal007 <lauragachanja007@gmail.com>
2025-02-05 08:37:10 +0300

    gsdgsdg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react"; // Mobile menu icons
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.jpg";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
